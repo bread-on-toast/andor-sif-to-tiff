a162c9a250288ecfd415e35ce1cc00b32c6e54ce
bread-on-toast <andre@allthefish.de>
2014-10-25 17:09:13 +0200

    initial commit

---
+++ siftotiff.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python
+
+#use hexdump to create an input-file
+
+import numpy as np
+
+def autocorr(x):
+    result = np.correlate(x, x, mode='full')
+    return result[result.size/2:]
+
+x=0
+y=0
+num=0
+a=[]
+with open("img") as f:
+	px=0
+	add=0
+	for i in f:
+		line=i.split(" ")
+		for word in line:
+			if px==1 and add==0:
+				num=num+1
+				px=0
+				print y,x,(int(word,16)/2**11)
+#				a.append(int(word,16))
+				y=y+1
+				if y==1536:#6 #1600+64 somehow
+					y=0
+					x=x+1
+			if px==1 and add==1:
+				add=0
+                                num=num+1
+                                px=0
+                                print x,y,(int("1"+word,16)/2**11)
+#                               a.append(int(word,16))
+                                y=y+1
+                                if y==1536:#6 #1600+64 somehow
+                                        y=0
+                                        x=x+1
+
+			if word=="0044": px=1
+			if  word=="0045":
+				px=1
+				add=1
+
+
+#import numpy as np
+
+#b=autocorr(a[10000:30000])
+#for i in b:
+#	print abs(i)
