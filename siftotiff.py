085991420fe3e8d67423fda946db74d73b5966cb
bread-on-toast <andre@allthefish.de>
2014-10-27 00:35:50 +0100

    fixed pixel detection

--- siftotiff.py
+++ siftotiff.py
@@ -42,13 +42,14 @@ while pos<len(rawdata)-1:
 		row=[]
 	pos=pos+2
 #print data
+f = open("img.pgm", "wb")
 
-print "P2"
-print len(data[0]),len(data),
-print 2**16-1
+f.write("P2\n")
+f.write(str(len(data[0]))+" "+str(len(data))+" "+str(2**16-1)+"\n")
 
 for i in data:
 	line=""
 	for j in i:
-		line=line+str(j)+" "
-	print line
+		line=line+str(j/2)+" "
+	f.write(line+"\n")
+f.close
