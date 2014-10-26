44e8774563021bf7c30b1acd6767f1da9c58a674
bread-on-toast <andre@allthefish.de>
2014-10-26 02:55:02 +0200

    rewritten output, producing pgm format, detection of first bit wrong -> errors arround 32768

--- siftotiff.py
+++ siftotiff.py
@@ -14,7 +14,8 @@ num=0
 a=[]
 lines=1535
 rows=2047
-
+data=[]
+row=[]
 with open("img") as f:
 	px=0
 	add=0
@@ -24,21 +25,25 @@ with open("img") as f:
 			if px==1 and add==0:
 				num=num+1
 				px=0
-				print y,x,(int(word,16)/2**11)
-#				a.append(int(word,16))
+				row.append(int(word,16))
+#				print y,x,(int(word,16)/2**11)
 				y=y+1
 				if y==1536:#6 #1600+64 somehow
+					data.append(row)
+					row=[]
 					y=0
 					x=x+1
 			if px==1 and add==1:
 				add=0
                                 num=num+1
                                 px=0
-                                print x,y,(int("1"+word,16)/2**11)
-#                               a.append(int(word,16))
+                                row.append(int("1"+word,16))
+                                #print y,x,(int("1"+word,16)/2**11)
                                 y=y+1
                                 if y==1536:#6 #1600+64 somehow
-                                        y=0
+                                        data.append(row)
+                                        row=[]
+					y=0
                                         x=x+1
 
 			if word=="0044": px=1
@@ -47,15 +52,14 @@ with open("img") as f:
 				add=1
 
 
-import numpy as np
-import Image
 
-data = np.random.randint(0, 255, (10,10)).astype(np.uint8)
-im = Image.fromarray(data)
-im.save('test.tif')
 
-#import numpy as np
+print "P2"
+print len(data[0]),len(data),
+print 2**16-1
 
-#b=autocorr(a[10000:30000])
-#for i in b:
-#	print abs(i)
+for i in data:
+	line=""
+	for j in i:
+		line=line+str(j/2)+" "
+	print line
